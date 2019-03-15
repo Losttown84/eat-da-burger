c77446c0a73da582f2e1bd67f5a1788564a56608
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-03-15 10:29:03 -0500

    fixed seeds and schema for db info

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE `burgers` (
- id int AUTO_INCREMENT,
-  burgers varchar(30) NOT NULL,
+ id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(100) NOT NULL,
+  devoured BOOLEAN default false,
   PRIMARY KEY(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
-INSERT into burgers (burgers
-) VALUES ("")
+INSERT into burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT into burgers (burger_name, devoured) VALUES ("Big Mac", false);
+INSERT into burgers (burger_name, devoured) VALUES ("Turkey Burger with Cheddar", true);
