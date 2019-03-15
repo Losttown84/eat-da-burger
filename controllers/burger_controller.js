6987865cd5fb968a8dcb525b11e1a37870106e77
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-03-15 18:32:39 -0500

    setting up routes for burgers

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+  burger.selectAll (function(data) {
+    var
+  })
+})
 
+router.post("/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name"
+  ], [
+    req.body.burger_name
+  ], function(data) {
+    res.redirect("/");
+  });
+});
 
-
+router.put("/burgers/:id", function(req, res) {
+  
+})
 
 module.exports = router;
